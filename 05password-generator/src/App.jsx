92e4a81e6d38d96d5b95d6420e689e47a1892157
siddhi-algude <siddhi.algude@mindstix.com>
2025-11-10 20:58:43 +0530

    final practice codes

--- 05password-generator/src/App.jsx
+++ 05password-generator/src/App.jsx
@@ -44,7 +44,7 @@ function App() {
   const [length, setLength] = useState(8)
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false)
-  const [password, setPassword] = useState("")
+  const [password, setPassword] = useState("") 
 
   //useRef hook
   const passwordRef = useRef(null)
